91e436de2ec85863129c07b1a7ae8632c9838018
RoboBronco <39787636+RoboBronco@users.noreply.github.com>
2020-07-03 00:38:21 -0600

    Update KnightBoard.java

--- p1/KnightBoard.java
+++ p1/KnightBoard.java
@@ -1,17 +1,26 @@
+import java.util.Arrays;
 
 public class KnightBoard {
 
-	private int board[][];
+	public int board[][];
 	int pos = 0;
 	int n;
-	public final int[] row = {2, 1, -1, -2, -2, -1,  1,  2};
 	public final int[] col = {1, 2,  2,  1, -1, -2, -2, -1};
+	public final int[] row = {2, 1, -1, -2, -2, -1,  1,  2};
 	
 	public KnightBoard(int n, int x, int y, int strat) {
 		this.n = n;
 		board = new int[n][n];
-		if(isValid(x, y)) {
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				board[i][j] = -1;
+			}
+		}
+		
+		if(isValid(x, y, board)) {
 			board[x][y] = pos;
+			System.out.println("Initial position (" + x + ", " + y + ") is:" + board[x][y]);
 		}
 		
 		if(strat == 0) {
@@ -21,22 +30,43 @@ public class KnightBoard {
 	
 	public void KnightBoardBasic(int board[][], int x, int y, int pos) {
 		
+		board[x][y] = pos;
+		System.out.println("position (" + x + ", " + y + ") is step: " + board[x][y] + "\n");
+		
+		if(pos >= n*n) {
+			print(board);
+			//board[x][y] = 0;
+			return;
+		}
+				
 		for(int k=0; k<8; k++) {
-			int nextX = x+row[k];
-			int nextY = y+col[y];
+			int nextX = x+col[k];
+			int nextY = y+row[y];
 			
-			if(isValid(nextX, nextY) && board[nextX][nextY]==0) {
-				KnightBoardBasic(board, nextX, nextY, pos+1);
+			System.out.println("Testing position (" + nextX + ", " + nextY + ")");
+			if(isValid(nextX, nextY, board)) {
+				pos++;
+				System.out.println("Next position is: (" + nextX + ", " + nextY + ")\n");
+				KnightBoardBasic(board, nextX, nextY, pos);
 			}
 		}
+		
+		System.out.println("Over 8");
+		board[x][y] = -1;
 
 	}
 
-	private boolean isValid(int x, int y) {
-		if(x<0 || x>=n || y<0 || y>=n) {
+	private boolean isValid(int x, int y, int[][] board) {
+		if(x<0 || x>=n || y<0 || y>=n || board[x][y]!=-1) {
 			return false;
 		}
 		return true;
 	}
-	
+
+	private static void print(int[][] board) {
+		for(var r: board) {
+			System.out.println(Arrays.toString(r));
+		}
+		System.out.println();
+	}
 }
