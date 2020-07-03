347835f97896e6c05e111327ed4ef68b5a4b27f7
RoboBronco <39787636+RoboBronco@users.noreply.github.com>
2020-07-03 00:38:06 -0600

    Update KnightTour.java

--- p1/KnightTour.java
+++ p1/KnightTour.java
@@ -1,8 +1,19 @@
 
 public class KnightTour {
-
+	
 	public static void main(String[] args) {
-
+		
+		int strat = Integer.parseInt(args[0]);
+		int n = Integer.parseInt(args[1]);
+		int x = Integer.parseInt(args[2]);
+		int y = Integer.parseInt(args[3]);
+		
+		//KnightBoard.KnightBoardBasic(n, x, y, strat);
+		//System.out.println(tour.board[1][1]);
+		
+		Kinght.startKnightTour(n, x, y);
+		
+		
 	}
 
 }
